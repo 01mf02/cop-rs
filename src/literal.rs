ed7b3a5c0400cfc9205669fd6e3995f296fa108e
Michael Färber <michael.faerber@gedenkt.at>
2020-10-19 10:05:49 +0200

    Fix negation equality test.

--- src/literal.rs
+++ src/literal.rs
@@ -30,8 +30,21 @@ impl<T> Neg for Signed<T> {
     }
 }
 
+impl<T: Eq> Signed<T> {
+    pub fn is_neg_of(&self, other: &Self) -> bool {
+        self.0 == !other.0 && self.1 == other.1
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Lit<C, V>(Signed<C>, Rc<Args<C, V>>);
+
+impl<C, V> Neg for Lit<C, V> {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self(-self.0, self.1)
+    }
+}
 
 impl<C: Display, V: Display> Display for Lit<C, V> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -51,7 +64,7 @@ impl<C, V> Lit<C, V> {
 
 impl<C: Eq, V: Eq> Lit<C, V> {
     pub fn is_neg_of(&self, other: &Self) -> bool {
-        self.0 != other.0 && self.1 == other.1
+        self.0.is_neg_of(&other.0) && self.1 == other.1
     }
 }
 
