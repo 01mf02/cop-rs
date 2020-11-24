a6ec35d646883b99eb5c9e0870e15f8d8be7470d
Michael Färber <01mf02@gmail.com>
2020-11-24 19:06:01 +0100

    Change order of RestIter.

--- src/rest_iter.rs
+++ src/rest_iter.rs
@@ -6,7 +6,7 @@
 /// let r: Vec<_> = RestIter::from(v)
 ///     .map(|(x, r)| (x, r.into_iter().collect()))
 ///     .collect();
-/// assert_eq!(r, vec![(3, vec![1, 2]), (2, vec![1, 3]), (1, vec![2, 3])])
+/// assert_eq!(r, vec![(1, vec![2, 3]), (2, vec![1, 3]), (3, vec![1, 2])])
 /// ~~~
 #[derive(Clone)]
 pub struct RestIter<T> {
@@ -28,10 +28,11 @@ impl<T> IntoIterator for Rest<T> {
 }
 
 impl<T> From<Vec<T>> for RestIter<T> {
-    fn from(v: Vec<T>) -> Self {
+    fn from(mut v: Vec<T>) -> Self {
+        v.reverse();
         Self {
-            left: v,
-            right: vec![],
+            left: vec![],
+            right: v,
         }
     }
 }
@@ -39,9 +40,9 @@ impl<T> From<Vec<T>> for RestIter<T> {
 impl<T: Clone> Iterator for RestIter<T> {
     type Item = (T, Rest<T>);
     fn next(&mut self) -> Option<Self::Item> {
-        let mid = self.left.pop()?;
+        let mid = self.right.pop()?;
         let rest = Rest(self.clone());
-        self.right.push(mid.clone());
+        self.left.push(mid.clone());
         Some((mid, rest))
     }
 }
