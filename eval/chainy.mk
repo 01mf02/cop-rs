4ad07722cac9c325a61c8744e225fd861fc03988
Michael Färber <01mf02@gmail.com>
2021-02-03 18:25:14 +0100

    Adapt Makefiles.

--- eval/chainy.mk
+++ eval/chainy.mk
@@ -2,10 +2,16 @@ include cop.mk
 
 CHAINY := $(shell find i/chainy/ -type f | sort -R)
 
-$(eval $(call cop-target,chainy,$(CHAINY),1,conj))
-$(eval $(call cop-target,chainy,$(CHAINY),1,conj cut))
-$(eval $(call cop-target,chainy,$(CHAINY),1,conj cut cutalt))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj --cutred))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj --cutext deep))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj --cutext shallow))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj --cutred --cutext deep))
+$(eval $(call leancop,chainy,$(CHAINY),1,--conj --cutred --cutext shallow))
 
-$(eval $(call cop-target,chainy,$(CHAINY),10,conj))
-$(eval $(call cop-target,chainy,$(CHAINY),10,conj cut))
-$(eval $(call cop-target,chainy,$(CHAINY),10,conj cut cutalt))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj --cutred))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj --cutext deep))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj --cutext shallow))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj --cutred --cutext deep))
+$(eval $(call leancop,chainy,$(CHAINY),10,--conj --cutred --cutext shallow))
--- eval/cop.mk
+++ eval/cop.mk
@@ -2,10 +2,10 @@ include join.mk
 
 TIME := /usr/bin/time --format '{"user": %U, "system": %S, "elapsed": "%E"}'
 CHECK := [ $$? -eq 124 ]
-COP := ../target/release/cop
+COP := ../target/release/leancop
 
 $(COP): .FORCE
-	cd .. && cargo build --release
+	cargo build --release
 .FORCE:
 
 # Create an evaluation target for the connection prover.
@@ -18,15 +18,15 @@ $(COP): .FORCE
 # 4. The arguments passed to the prover.
 #
 # For example,
-#     $(eval $(call cop-target,bushy,$(BUSHY),1,--conj --cut deep))
+#     $(eval $(call leancop,bushy,$(BUSHY),1,--conj --cut deep))
 # creates a target `o/bushy/1s/cop--conj--cutdeep` that evaluates
 # the dataset in `i/bushy` with a 1 second timeout,
 # using the flags `--conj --cut deep`.
 #
 # See <https://www.gnu.org/software/make/manual/make.html#Eval-Function>
 # for how the combination of `define`, `call`, and `eval` works.
-define cop-target =
-OUT = o/$(1)/$(3)s/cop$$(call join-with,,$(4))
+define leancop =
+OUT = o/$(1)/$(3)s/leancop$$(call join-with,,$(4))
 
 .PHONY: $$(OUT)
 $$(OUT): $$(patsubst i/$(1)/%,$$(OUT)/%,$(2))
