4843487af9d452400e05b23662e98235d8dd57c9
Michael Färber <01mf02@gmail.com>
2021-02-23 11:02:32 +0100

    100s TPTP evaluation.

--- eval/tptp.mk
+++ eval/tptp.mk
@@ -4,3 +4,4 @@ TPTP630FOF := $(shell find i/TPTP-v6.3.0/ -type f -name "*+?.p" | sort -R)
 
 $(foreach s,$(STRATEGIES),$(eval $(call leancop,TPTP-v6.3.0,$(TPTP630FOF),1, $(call undot,$(s)))))
 $(foreach s,$(STRATEGIES),$(eval $(call leancop,TPTP-v6.3.0,$(TPTP630FOF),10,$(call undot,$(s)))))
+$(foreach s,$(STRATEGIES),$(eval $(call leancop,TPTP-v6.3.0,$(TPTP630FOF),100,$(call undot,$(s)))))
