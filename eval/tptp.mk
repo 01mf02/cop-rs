5328a038a546cdcf73880e0f5fd4e6b75c5d6227
Michael Färber <01mf02@gmail.com>
2021-04-08 16:27:57 +0200

    Remove unused 100s TPTP evaluation.

--- eval/tptp.mk
+++ eval/tptp.mk
@@ -4,4 +4,3 @@ TPTP630FOF := $(shell find i/TPTP-v6.3.0/ -type f -name "*+?.p" | sort -R)
 
 $(foreach s,$(STRATEGIES),$(eval $(call meancop,TPTP-v6.3.0,$(TPTP630FOF),1, $(call undot,$(s)))))
 $(foreach s,$(STRATEGIES),$(eval $(call meancop,TPTP-v6.3.0,$(TPTP630FOF),10,$(call undot,$(s)))))
-$(foreach s,$(STRATEGIES),$(eval $(call meancop,TPTP-v6.3.0,$(TPTP630FOF),100,$(call undot,$(s)))))
