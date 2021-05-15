84981b5f91f0ee950bc6753ca16fea3dc47239e5
Michael Färber <michael.faerber@gedenkt.at>
2021-05-15 15:00:56 +0200

    Make some Markdown scripts read files directly.

--- eval/markdown/ratio-infs.sh
+++ eval/markdown/ratio-infs.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Expects lines of the format:
 # {"C1": "None", "C2": "None", "C3": "REX", ...}
+IN=json/infs.json
+if [ ! -e $IN ]
+then
+  echo Please run json/proofs.sh to generate $IN first.
+  exit 1
+fi
+
 SCRIPT='transpose | .[0][], (.[3:][] | .[0] / .[1] * 10 | round / 10)'
+cat $IN |
 jq -s \
 ' [[[.[0][]], [.[1][]]] | '"$SCRIPT"']'\
 ',[[[.[2][]], [.[3][]]] | '"$SCRIPT"']'\
--- eval/markdown/ratio-problems.sh
+++ eval/markdown/ratio-problems.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+IN=json/problems.json
+if [ ! -e $IN ]
+then
+  echo Please run json/proofs.sh to generate $IN first.
+  exit 1
+fi
+
 SCRIPT='transpose | .[0][], (.[2:][] | .[1] / .[0] * 1000 | round / 10)'
+cat $IN |
 jq -s \
 ' [[[.[0][]], [.[1][]]] | '"$SCRIPT"']'\
 ',[[[.[0][]], [.[2][]]] | '"$SCRIPT"']'\
