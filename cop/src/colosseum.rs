d36d7ea6649f0e9087e52d8f302af10528de2ebf
Michael Färber <01mf02@gmail.com>
2021-05-26 18:56:47 +0200

    Fix test.

--- cop/src/colosseum.rs
+++ cop/src/colosseum.rs
@@ -6,22 +6,6 @@ use core::ops::Deref;
 use hashbrown::HashSet;
 
 /// Return equal pointers for any two equal dereferenceable objects.
-///
-/// ~~~
-/// # use cop::ptr::normalise;
-/// use colosseum::unsync::Arena;
-/// use hashbrown::HashSet;
-///
-/// let s1 = "Hello".to_string();
-/// let s2 = "Hello".to_string();
-/// assert!(!(core::ptr::eq(&*s1, &*s2)));
-///
-/// let arena: Arena<String> = Arena::new();
-/// let mut set: HashSet<&str> = HashSet::new();
-/// let p1 = normalise(s1, &arena, &mut set);
-/// let p2 = normalise(s2, &arena, &mut set);
-/// assert!(core::ptr::eq(p1, p2));
-/// ~~~
 fn normalise<'a, T, Q>(x: T, arena: &'a Arena<T>, set: &mut HashSet<&'a Q>) -> &'a Q
 where
     T: Deref<Target = Q>,
@@ -46,4 +30,18 @@ impl<V> Form<String, String, V> {
         let mut symb = |s| Symbol::new(normalise(s, arena, set));
         self.map_predicates(&mut symb).map_constants(&mut symb)
     }
+}
+
+#[test]
+fn normalise_string() {
+    use alloc::string::ToString;
+    let s1 = "Hello".to_string();
+    let s2 = "Hello".to_string();
+    assert!(!(core::ptr::eq(&*s1, &*s2)));
+
+    let arena: Arena<String> = Arena::new();
+    let mut set: HashSet<&str> = HashSet::new();
+    let p1 = normalise(s1, &arena, &mut set);
+    let p2 = normalise(s2, &arena, &mut set);
+    assert!(core::ptr::eq(p1, p2));
 }
