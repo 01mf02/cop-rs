b8036c42a9ee4d360902954a13d96306bd73be3c
Michael Färber <01mf02@gmail.com>
2021-05-27 10:07:19 +0200

    Nicer equality handling.

--- cop/src/equality.rs
+++ cop/src/equality.rs
@@ -1,5 +1,5 @@
-use crate::term::{Args, Arity};
-use crate::{Form, Term};
+use crate::fof::{Form, OpA};
+use crate::term::{Args, Arity, Term};
 use alloc::vec::Vec;
 
 impl<P, C> Form<P, C, usize> {
@@ -23,25 +23,19 @@ impl<P, C> Form<P, C, usize> {
         Form::foralls(vars, Form::imp(xy & yz, xz))
     }
 
-    /// Return `0 = 1 & (2 = 3 & (..))`, consisting of n conjuncts.
-    ///
-    /// Return `None` if `n` is 0.
-    fn equalities(n: Arity) -> Option<Self> {
-        // TODO: use simply BinA here?
-        let prems = (0..n).map(|v| Form::EqTm(Term::V(2 * v), Term::V(1 + 2 * v)));
-        let mut prems = prems.rev();
-        let last = prems.next()?;
-        Some(prems.fold(last, |acc, eq| eq & acc))
-    }
-
     fn eq_subst<F>(arity: Arity, f: F) -> Option<Self>
     where
         F: FnOnce(Self, Args<C, usize>, Args<C, usize>) -> Self,
     {
-        let eqs = Self::equalities(arity)?;
+        if arity == 0 {
+            return None;
+        }
+        // construct `0 = 1 & (2 = 3 & (..))`, consisting of `arity` conjuncts
+        let eq = (0..arity).map(|v| Form::EqTm(Term::V(2 * v), Term::V(1 + 2 * v)));
+        let eq = Self::BinA(OpA::Conj, eq.collect());
         let al = (0..arity).map(|v| Term::V(2 * v)).collect();
         let ar = (0..arity).map(|v| Term::V(1 + 2 * v)).collect();
-        Some(Form::foralls((0..2 * arity).rev(), f(eqs, al, ar)))
+        Some(Form::foralls((0..2 * arity).rev(), f(eq, al, ar)))
     }
 }
 
@@ -68,7 +62,6 @@ impl<P: Clone, C: Clone> Form<P, C, usize> {
     /// `((refl & sym & trans) & p1 & ... & pn) & c1 & ... & cm`,
     /// where the conjunction is associated to the right.
     pub fn eq_axioms(preds: Vec<(&P, Arity)>, consts: Vec<(&C, Arity)>) -> Self {
-        use crate::fof::OpA;
         let init = Self::eq_refl() & (Self::eq_sym() & Self::eq_trans());
         let p = preds
             .into_iter()
@@ -78,4 +71,23 @@ impl<P: Clone, C: Clone> Form<P, C, usize> {
             .filter_map(|(c, arity)| Self::eq_constant(c, arity));
         (init & Form::binas(OpA::Conj, p)) & Form::binas(OpA::Conj, c)
     }
+}
+
+#[test]
+fn eq_constant() {
+    use Form::EqTm;
+    use Term::{C, V};
+
+    // ![0, 1, 2, 3]: (0 = 1 & 2 = 3) => f(0, 2) = f(1, 3)
+    let f = "f";
+    let premise = EqTm(V(0), V(1)) & EqTm(V(2), V(3));
+    let concl = EqTm(
+        C(f, Args::from([V(0), V(2)])),
+        C(f, Args::from([V(1), V(3)])),
+    );
+    let out = Form::foralls((0..4).rev(), Form::imp(premise, concl));
+    assert_eq!(Form::eq_constant(&f, 2), Some::<Form<(), _, _>>(out));
+
+    // for a nullary function symbol, there is no substitution axiom
+    assert_eq!(Form::eq_constant(&f, 0), None::<Form<(), _, _>>);
 }
