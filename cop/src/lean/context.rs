f25437652a29d24a26ecf7464935d4c6eb99ba54
Michael Färber <01mf02@gmail.com>
2021-10-15 09:39:52 +0200

    Simplify context pointers.

--- cop/src/lean/context.rs
+++ cop/src/lean/context.rs
@@ -1,32 +1,28 @@
 use crate::Rewind;
 use alloc::vec::Vec;
 
-#[derive(Clone, Default)]
+#[derive(Copy, Clone, Default)]
 pub struct Context<T> {
     pub path: T,
     pub lemmas: T,
 }
 
-#[derive(Copy, Clone)]
-pub struct Ptr {
-    path_len: usize,
-    lemmas_len: usize,
-}
+pub type Ptr = Context<usize>;
 
 impl<'t, T> From<&Context<Vec<T>>> for Ptr {
     fn from(ctx: &Context<Vec<T>>) -> Self {
         Self {
-            path_len: ctx.path.len(),
-            lemmas_len: ctx.lemmas.len(),
+            path: ctx.path.len(),
+            lemmas: ctx.lemmas.len(),
         }
     }
 }
 
 impl<T> Rewind<Ptr> for Context<Vec<T>> {
     fn rewind(&mut self, ptr: Ptr) {
-        assert!(self.path.len() >= ptr.path_len);
-        assert!(self.lemmas.len() >= ptr.lemmas_len);
-        self.path.truncate(ptr.path_len);
-        self.lemmas.truncate(ptr.lemmas_len);
+        assert!(self.path.len() >= ptr.path);
+        assert!(self.lemmas.len() >= ptr.lemmas);
+        self.path.truncate(ptr.path);
+        self.lemmas.truncate(ptr.lemmas);
     }
 }
